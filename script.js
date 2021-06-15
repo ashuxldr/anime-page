491bf8adf26a9cb04c25c14b05c9b8009755a1b6
ashuxldr <ashuxldr@gmail.com>
2021-06-15 15:33:02 +0530

    modified chatbox

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 const chatbox = document.querySelector('.chatbox')
-const active = document.getElementById('img1')
-// setInterval(function () {
-//     $('.puffIn .magictime').toggleClass('puffOut');
-// }, 50);
+const animate = document.getElementById('animateButton')
+
 function toggle() {
+
     // Audio on Click
-    var audio = new Audio("Beep.mp3");
+    // var audio = new Audio("Beep.mp3");
+    var audio = new Audio("bubble.mp3");
+    // var audio = new Audio("water.mp3");
     audio.play();
 
     // Display Or Hide Chatbox
+
+    // $('.puffIn').toggleClass('puffOut');
     if (chatbox.style.display == "none") {
         chatbox.style.display = "block";
-        active.classList.add('animate');
+        animate.classList.add('animate');
     }
     else {
-        active.classList.remove('animate')
+        animate.classList.remove('animate')
         chatbox.style.display = "none";
     }
 
 }
+
+// Typing feature on keypress
+const demo = document.getElementById('demo')
+$(document).ready(function () {
+    $("input").keydown(function () {
+        console.log('pressed down')
+        demo.innerHTML = "Typing..."
+        demo.style.display = "block"
+
+    });
+});
+
+$(document).ready(function () {
+    $("input").keyup(function () {
+        console.log('pressed up')
+        demo.innerHTML = ""
+        demo.style.display = "none"
+        // demo.classList.add = "user"
+    });
+});
 
